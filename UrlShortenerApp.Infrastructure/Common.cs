9eafb3a423f03e5f747e55c9d0631dc27a75bbb5
lukasurmava <surmava0@gmail.com>
2025-10-22 16:20:32 +0400

    Added GenerateShortCode method

--- UrlShortenerApp.Infrastructure/Common.cs
+++ UrlShortenerApp.Infrastructure/Common.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace UrlShortenerApp.Infrastructure
 {
-    internal class Common
+    public class Common
     {
+        static string GenerateShortCode()
+        {
+            string alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string code = "";
+            var length = new Random().Next(6, 9);
+            var random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(0, alphabet.Length);
+                code += alphabet[index];
+            }
+
+            return code;
+        }
     }
 }
