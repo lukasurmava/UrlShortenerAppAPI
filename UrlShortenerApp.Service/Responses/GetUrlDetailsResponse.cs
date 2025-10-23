e1e4e8401949759cd58dae7f360ea6497c62aec5
lukasurmava <surmava0@gmail.com>
2025-10-23 11:05:11 +0400

    Added GetUrlDetailsResponse

---
+++ UrlShortenerApp.Service/Responses/GetUrlDetailsResponse.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortenerApp.Domain;
+
+namespace UrlShortenerApp.Service.Responses
+{
+    public class GetUrlDetailsResponse
+    {
+        public string OriginalUrl { get; set; }
+        public string ShortCode { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public bool IsActive { get; set; }
+        public int ClickCount { get; set; }
+        public List<Analytic> Analytics { get; set; }
+    }
+}
