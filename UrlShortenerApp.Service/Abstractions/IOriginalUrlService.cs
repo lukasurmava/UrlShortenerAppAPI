be63ed2650108a9d9848aac379638e5d223fdc17
lukasurmava <surmava0@gmail.com>
2025-10-23 11:56:36 +0400

    Updated IOriginalUrlService

--- UrlShortenerApp.Service/Abstractions/IOriginalUrlService.cs
+++ UrlShortenerApp.Service/Abstractions/IOriginalUrlService.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UrlShortenerApp.Service.Requests;
+using UrlShortenerApp.Service.Responses;
 
 namespace UrlShortenerApp.Service.Abstractions
 {
     public interface IOriginalUrlService
     {
-        public Task CreateShortUrl();
+        public Task GetByShortCode(string shortCode);
+        public Task<CreateOriginalUrlResponse> CreateShortUrl(CreateOriginalUrlRequest request);
+        public Task<GetUrlDetailsResponse> GetUrlDetails(string shortCode);
+
+        public Task UpdateUrl(UpdateOriginalUrlRequest request);
+        public Task DeleteUrl(string shortCode);
     }
 }
