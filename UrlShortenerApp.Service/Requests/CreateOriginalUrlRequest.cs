78d081173184f6d69aef52ab0d2d866a5d0d736b
lukasurmava <surmava0@gmail.com>
2025-10-23 10:48:16 +0400

    Added CreateOriginalUrlRequest

--- UrlShortenerApp.Service/Requests/CreateOriginalUrlRequest.cs
+++ UrlShortenerApp.Service/Requests/CreateOriginalUrlRequest.cs
@@ -8,5 +8,8 @@ namespace UrlShortenerApp.Service.Requests
 {
     public class CreateOriginalUrlRequest
     {
+        public string OriginalUrl { get; set; }
+        public DateTime? ExpireDate { get; set; }
+        public string? Alias { get; set; }
     }
 }
