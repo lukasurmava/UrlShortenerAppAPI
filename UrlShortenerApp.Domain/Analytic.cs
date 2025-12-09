b84e5e3a414a8d2df945d54b5bbe330a89ef294e
lukasurmava <surmava0@gmail.com>
2025-12-09 14:52:59 +0400

    added Id in analytic

--- UrlShortenerApp.Domain/Analytic.cs
+++ UrlShortenerApp.Domain/Analytic.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UrlShortenerApp.Domain
 {
-    public class Analytic : Entity
+    public class Analytic
     {
+        [Key]
+        public int Id { get; set; }
+        public string ShortCode { get; set; }
         public DateTime ClickDate { get; set; }
         public string UserAgent { get; set; }
         public string IpAdress { get; set; }
