ee208b633b9ca64408c598db0145c52cc6a939f7
lukasurmava <surmava0@gmail.com>
2025-12-10 13:59:30 +0400

    refactored Analytic

--- UrlShortenerApp.Domain/Analytic.cs
+++ UrlShortenerApp.Domain/Analytic.cs
@@ -9,11 +9,31 @@ namespace UrlShortenerApp.Domain
 {
     public class Analytic
     {
+        private Analytic() { }
+        public Analytic(string shortCode, string userAgent, string ipAdress)
+        {
+            if(string.IsNullOrWhiteSpace(shortCode))
+            {
+                throw new ArgumentException("Short code cannot be null or empty.", nameof(shortCode));
+            }
+            if(string.IsNullOrWhiteSpace(userAgent))
+            {
+                throw new ArgumentException("User agent cannot be null or empty.", nameof(userAgent));
+            }
+            if(string.IsNullOrWhiteSpace(ipAdress))
+            {
+                throw new ArgumentException("IP address cannot be null or empty.", nameof(ipAdress));
+            }
+            ShortCode = shortCode;
+            ClickDate = DateTime.UtcNow;
+            UserAgent = userAgent;
+            IpAdress = ipAdress;
+        }
         [Key]
-        public int Id { get; set; }
-        public string ShortCode { get; set; }
-        public DateTime ClickDate { get; set; }
-        public string UserAgent { get; set; }
-        public string IpAdress { get; set; }
+        public int Id { get; private set; }
+        public string ShortCode { get; private set; }
+        public DateTime ClickDate { get; private set; }
+        public string UserAgent { get; private set; }
+        public string IpAdress { get; private set; }
     }
 }
