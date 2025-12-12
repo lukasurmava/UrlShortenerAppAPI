9bdfbc2f53241c5f80a4a27c7c9af4b433a7e5ef
lukasurmava <surmava0@gmail.com>
2025-12-12 17:52:49 +0400

    Added FailedResponse

---
+++ UrlShortenerApp.Service/Responses/FailedResponse.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrlShortenerApp.Service.Responses
+{
+    public record FailedResponse(string Error) : ResponseBase(false, Error);
+}
